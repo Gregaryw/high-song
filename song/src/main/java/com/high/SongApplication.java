caa128c5cfa2b9ceb5911552953da889aafa1d41
wanglicheng <1623682972@qq.com>
2019-03-21 18:15:30 +0800

    添加基础模块

---
+++ song/src/main/java/com/high/SongApplication.java
@@ -0,0 +1,17 @@
+package com.high;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 容器启动入口
+ * @Author: HarlanW
+ * @Date: 2019/3/21 0021
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class SongApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SongApplication.class,args);
+    }
+}
---
+++ user/src/main/java/com/high/UserApplication.java
@@ -0,0 +1,17 @@
+package com.high;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 容器启动类入口
+ * @Author: HarlanW
+ * @Date: 2019/3/21
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
+}
